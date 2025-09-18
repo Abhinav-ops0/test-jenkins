8bb465aa280625be9bcef6be00927f5cc1dccb92
Abhinav-ops0 <Abhinav.Prakash@ops0.com>
2025-09-18 18:32:37 +0530

    Add Terraform infrastructure: I'll help you create an S3 bucket named "test-new-...

--- configs/infrastructure/outputs.tf
+++ configs/infrastructure/outputs.tf
@@ -2,21 +2,21 @@
 # This file contains all output values from the Terraform configuration
 
 output "bucket_name" {
-  value       = aws_s3_bucket.test_logic_bucket.id
+  value       = aws_s3_bucket.test_repo_bucket.id
   description = "The name of the bucket"
 }
 
 output "bucket_arn" {
-  value       = aws_s3_bucket.test_logic_bucket.arn
+  value       = aws_s3_bucket.test_repo_bucket.arn
   description = "The ARN of the bucket"
 }
 
 output "bucket_domain_name" {
-  value       = aws_s3_bucket.test_logic_bucket.bucket_domain_name
+  value       = aws_s3_bucket.test_repo_bucket.bucket_domain_name
   description = "The bucket domain name"
 }
 
 output "bucket_regional_domain_name" {
-  value       = aws_s3_bucket.test_logic_bucket.bucket_regional_domain_name
+  value       = aws_s3_bucket.test_repo_bucket.bucket_regional_domain_name
   description = "The bucket region-specific domain name"
 }
